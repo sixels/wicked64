23b028846579563f01159cf89e6beb6283a244d2
Sixels <sixels@protonmail.com>
2022-05-04 14:30:53 -0300

    Separate physical and virtual addresses

--- wicked64-core/src/mmu/map.rs
+++ wicked64-core/src/mmu/map.rs
@@ -1,37 +1,43 @@
-#[rustfmt::skip]
 pub mod addr_map {
     use std::ops::RangeInclusive;
-
     type AddrRange = RangeInclusive<usize>;
 
-    pub static VIRT_KUSEG_RANGE: AddrRange = 0x00000000..=0x7FFFFFFF;
-    pub static VIRT_KSEG0_RANGE: AddrRange = 0x80000000..=0x9FFFFFFF;
-    pub static VIRT_KSEG1_RANGE: AddrRange = 0xA0000000..=0xBFFFFFFF;
-    pub static VIRT_KSSEG_RANGE: AddrRange = 0xC0000000..=0xDFFFFFFF;
-    pub static VIRT_KSEG3_RANGE: AddrRange = 0xE0000000..=0xFFFFFFFF;
+    pub mod virt {
+        use super::AddrRange;
 
-    pub const PHYS_RDRAM_RANGE: AddrRange          = 0x00000000..=0x007FFFFF;
-    pub const PHYS_RDRAM_REG_RANGE: AddrRange      = 0x03F00000..=0x03FFFFFF;
-    pub const PHYS_SP_DMEM_RANGE: AddrRange        = 0x04000000..=0x04000FFF;
-    pub const PHYS_SP_IMEM_RANGE: AddrRange        = 0x04001000..=0x04001FFF;
-    pub const PHYS_SP_REG_RANGE: AddrRange         = 0x04040000..=0x040FFFFF;
-    pub const PHYS_DP_CMD_REG_RANGE: AddrRange     = 0x04100000..=0x041FFFFF;
-    pub const PHYS_DP_SPAN_REG_RANGE: AddrRange    = 0x04200000..=0x042FFFFF;
-    pub const PHYS_MIPS_INT_RANGE: AddrRange       = 0x04300000..=0x043FFFFF;
-    pub const PHYS_VIDEO_INT_RANGE: AddrRange      = 0x04400000..=0x044FFFFF;
-    pub const PHYS_AUDIO_INT_RANGE: AddrRange      = 0x04500000..=0x045FFFFF;
-    pub const PHYS_PERIPHERAL_INT_RANGE: AddrRange = 0x04600000..=0x046FFFFF;
-    pub const PHYS_RDRAM_INT_RANGE: AddrRange      = 0x04700000..=0x047FFFFF;
-    pub const PHYS_SERIAL_INT_RANGE: AddrRange     = 0x04800000..=0x048FFFFF;
-    pub const PHYS_CART_D2A1_RANGE: AddrRange      = 0x05000000..=0x05FFFFFF;
-    pub const PHYS_CART_D1A1_RANGE: AddrRange      = 0x06000000..=0x07FFFFFF;
-    pub const PHYS_CART_D2A2_RANGE: AddrRange      = 0x08000000..=0x0FFFFFFF;
-    pub const PHYS_CART_D1A2_RANGE: AddrRange      = 0x10000000..=0x1FBFFFFF;
-    pub const PHYS_PIF_ROM_RANGE: AddrRange        = 0x1FC00000..=0x1FC007BF;
-    pub const PHYS_PIF_RAM_RANGE: AddrRange        = 0x1FC007C0..=0x1FC007FF;
-    pub const PHYS_RESERVED_RANGE: AddrRange       = 0x1FC00800..=0x1FCFFFFF;
-    pub const PHYS_CART_D1A3_RANGE: AddrRange      = 0x1FD00000..=0x7FFFFFFF;
-    pub const PHYS_UNKNOWN_RANGE: AddrRange        = 0x80000000..=0xFFFFFFFF;
+        pub static KUSEG_RANGE: AddrRange = 0x00000000..=0x7FFFFFFF;
+        pub static KSEG0_RANGE: AddrRange = 0x80000000..=0x9FFFFFFF;
+        pub static KSEG1_RANGE: AddrRange = 0xA0000000..=0xBFFFFFFF;
+        pub static KSSEG_RANGE: AddrRange = 0xC0000000..=0xDFFFFFFF;
+        pub static KSEG3_RANGE: AddrRange = 0xE0000000..=0xFFFFFFFF;
+    }
+
+    pub mod phys {
+        use super::AddrRange;
+
+        pub const RDRAM_RANGE: AddrRange = 0x00000000..=0x007FFFFF;
+        pub const RDRAM_REG_RANGE: AddrRange = 0x03F00000..=0x03FFFFFF;
+        pub const SP_DMEM_RANGE: AddrRange = 0x04000000..=0x04000FFF;
+        pub const SP_IMEM_RANGE: AddrRange = 0x04001000..=0x04001FFF;
+        pub const SP_REG_RANGE: AddrRange = 0x04040000..=0x040FFFFF;
+        pub const DP_CMD_REG_RANGE: AddrRange = 0x04100000..=0x041FFFFF;
+        pub const DP_SPAN_REG_RANGE: AddrRange = 0x04200000..=0x042FFFFF;
+        pub const MIPS_INT_RANGE: AddrRange = 0x04300000..=0x043FFFFF;
+        pub const VIDEO_INT_RANGE: AddrRange = 0x04400000..=0x044FFFFF;
+        pub const AUDIO_INT_RANGE: AddrRange = 0x04500000..=0x045FFFFF;
+        pub const PERIPHERAL_INT_RANGE: AddrRange = 0x04600000..=0x046FFFFF;
+        pub const RDRAM_INT_RANGE: AddrRange = 0x04700000..=0x047FFFFF;
+        pub const SERIAL_INT_RANGE: AddrRange = 0x04800000..=0x048FFFFF;
+        pub const CART_D2A1_RANGE: AddrRange = 0x05000000..=0x05FFFFFF;
+        pub const CART_D1A1_RANGE: AddrRange = 0x06000000..=0x07FFFFFF;
+        pub const CART_D2A2_RANGE: AddrRange = 0x08000000..=0x0FFFFFFF;
+        pub const CART_D1A2_RANGE: AddrRange = 0x10000000..=0x1FBFFFFF;
+        pub const PIF_ROM_RANGE: AddrRange = 0x1FC00000..=0x1FC007BF;
+        pub const PIF_RAM_RANGE: AddrRange = 0x1FC007C0..=0x1FC007FF;
+        pub const RESERVED_RANGE: AddrRange = 0x1FC00800..=0x1FCFFFFF;
+        pub const CART_D1A3_RANGE: AddrRange = 0x1FD00000..=0x7FFFFFFF;
+        pub const UNKNOWN_RANGE: AddrRange = 0x80000000..=0xFFFFFFFF;
+    }
 }
 
 /// Unfortunately, Rust [does not let we match const ranges](https://github.com/rust-lang/rust/issues/76191)
@@ -78,11 +84,11 @@ impl From<usize> for VirtualMemoryMap {
         use addr_map::*;
 
         map_range!(addr, {
-            VIRT_KUSEG_RANGE => Self::KUSEG,
-            VIRT_KSEG0_RANGE => Self::KSEG0,
-            VIRT_KSEG1_RANGE => Self::KSEG1,
-            VIRT_KSSEG_RANGE => Self::KSSEG,
-            VIRT_KSEG3_RANGE => Self::KSEG3,
+            virt::KUSEG_RANGE => Self::KUSEG,
+            virt::KSEG0_RANGE => Self::KSEG0,
+            virt::KSEG1_RANGE => Self::KSEG1,
+            virt::KSSEG_RANGE => Self::KSSEG,
+            virt::KSEG3_RANGE => Self::KSEG3,
             0x100000000..=usize::MAX => panic!("Memory offset '0x{:08x}' out of bound", addr),
             _ => unreachable!()
         })
@@ -156,28 +162,28 @@ impl From<usize> for PhysicalMemoryMap {
         use addr_map::*;
 
         map_range!(addr, {
-            PHYS_RDRAM_RANGE => Self::RDRAM,
-            PHYS_RDRAM_REG_RANGE => Self::RDRAMReg,
-            PHYS_SP_DMEM_RANGE => Self::SPDMEM(addr - *PHYS_SP_DMEM_RANGE.start()),
-            PHYS_SP_IMEM_RANGE => Self::SPIMEM,
-            PHYS_SP_REG_RANGE => Self::SPReg,
-            PHYS_DP_CMD_REG_RANGE => Self::DPCmdReg,
-            PHYS_DP_SPAN_REG_RANGE => Self::DPSpanReg,
-            PHYS_MIPS_INT_RANGE => Self::MIPSInterface,
-            PHYS_VIDEO_INT_RANGE => Self::VideoInterface,
-            PHYS_AUDIO_INT_RANGE => Self::AudioInterface,
-            PHYS_PERIPHERAL_INT_RANGE => Self::PeripheralInterface,
-            PHYS_RDRAM_INT_RANGE => Self::RDRAMInterface,
-            PHYS_SERIAL_INT_RANGE => Self::SerialInterface,
-            PHYS_CART_D2A1_RANGE => Self::CartridgeD2A1,
-            PHYS_CART_D1A1_RANGE => Self::CartridgeD1A1,
-            PHYS_CART_D2A2_RANGE => Self::CartridgeD2A2,
-            PHYS_CART_D1A2_RANGE => Self::CartridgeD1A2(addr - *PHYS_CART_D1A2_RANGE.start()),
-            PHYS_PIF_ROM_RANGE => Self::PIFROM,
-            PHYS_PIF_RAM_RANGE => Self::PIFRAM(addr - *PHYS_PIF_RAM_RANGE.start()),
-            PHYS_RESERVED_RANGE => Self::Reserved,
-            PHYS_CART_D1A3_RANGE => Self::CartridgeD1A3,
-            PHYS_UNKNOWN_RANGE => Self::ExternSysAD,
+            phys::RDRAM_RANGE => Self::RDRAM,
+            phys::RDRAM_REG_RANGE => Self::RDRAMReg,
+            phys::SP_DMEM_RANGE => Self::SPDMEM(addr - *phys::SP_DMEM_RANGE.start()),
+            phys::SP_IMEM_RANGE => Self::SPIMEM,
+            phys::SP_REG_RANGE => Self::SPReg,
+            phys::DP_CMD_REG_RANGE => Self::DPCmdReg,
+            phys::DP_SPAN_REG_RANGE => Self::DPSpanReg,
+            phys::MIPS_INT_RANGE => Self::MIPSInterface,
+            phys::VIDEO_INT_RANGE => Self::VideoInterface,
+            phys::AUDIO_INT_RANGE => Self::AudioInterface,
+            phys::PERIPHERAL_INT_RANGE => Self::PeripheralInterface,
+            phys::RDRAM_INT_RANGE => Self::RDRAMInterface,
+            phys::SERIAL_INT_RANGE => Self::SerialInterface,
+            phys::CART_D2A1_RANGE => Self::CartridgeD2A1,
+            phys::CART_D1A1_RANGE => Self::CartridgeD1A1,
+            phys::CART_D2A2_RANGE => Self::CartridgeD2A2,
+            phys::CART_D1A2_RANGE => Self::CartridgeD1A2(addr - *phys::CART_D1A2_RANGE.start()),
+            phys::PIF_ROM_RANGE => Self::PIFROM,
+            phys::PIF_RAM_RANGE => Self::PIFRAM(addr - *phys::PIF_RAM_RANGE.start()),
+            phys::RESERVED_RANGE => Self::Reserved,
+            phys::CART_D1A3_RANGE => Self::CartridgeD1A3,
+            phys::UNKNOWN_RANGE => Self::ExternSysAD,
 
             // unused ranges
             0x00800000..=0x03EFFFFF => Self::Unused(addr),
