00df0f70f6aad3aa8a7c0ac0d1d8ae19e3e70f9f
Sixels <sixels@protonmail.com>
2022-07-09 18:29:52 +0000

    Beginning the JIT code generator

--- wicked64-core/src/jit/codegen/emitter.rs
+++ wicked64-core/src/jit/codegen/emitter.rs
@@ -2,8 +2,15 @@ use std::io;
 
 use byteorder::WriteBytesExt;
 
+use super::register::X64Gpr;
+
 pub trait Emitter: io::Write {
-    // TODO
+    fn emit_push_reg(&mut self, reg: X64Gpr) -> io::Result<()> {
+        self.write_u8(0x50 + reg as u8)
+    }
+    fn emit_pop_reg(&mut self, reg: X64Gpr) -> io::Result<()> {
+        self.write_u8(0x58 + reg as u8)
+    }
 }
 
 impl<T: io::Write> Emitter for T {}
