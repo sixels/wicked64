ad15bd562ac6ad8d0d1b1271eb27d7fe19c1d38a
Sixels <sixels@protonmail.com>
2022-07-19 17:35:37 +0000

    Refactor cache

--- wicked64-core/src/jit/cache.rs
+++ wicked64-core/src/jit/cache.rs
@@ -1,41 +1,42 @@
-use std::{
-    ops::Range,
-    sync::{Arc, Mutex, RwLock},
-};
+use std::{cell::RefCell, collections::BTreeMap, rc::Rc};
 
-use crate::n64::SyncState;
+use crate::jit::{code::CompiledBlock, engine::JitEngine};
+use crate::n64::State;
 
-use super::{code::CompiledBlock, engine::JitEngine};
+#[derive(PartialEq, Eq, Clone, Copy)]
+enum CacheRange {
+    Start,
+    End,
+}
 
 pub struct Cache {
     jit_engine: JitEngine,
-    blocks: Mutex<hashbrown::HashMap<u64, Arc<CompiledBlock>>>,
-    compiled_ranges: RwLock<hashbrown::HashSet<Range<u64>>>,
+    blocks: hashbrown::HashMap<usize, CompiledBlock>,
+    compiled_addrs: BTreeMap<usize, CacheRange>,
 }
 
 impl Cache {
     /// Get a compiled block from the cache or create if no entries were found
-    pub fn get_or_compile(&self, addr: u64, state: SyncState) -> Arc<CompiledBlock> {
-        let mut blocks = self.blocks.lock().unwrap();
-
-        let block = blocks.entry(addr).or_insert_with(|| {
+    pub fn get_or_compile(&mut self, addr: usize, state: Rc<RefCell<State>>) -> &CompiledBlock {
+        let block = self.blocks.entry(addr).or_insert_with(|| {
             tracing::debug!("Generating cache for address '0x{addr:08x}'");
 
-            let block = self.jit_engine.compile_block(state);
-            let mut ranges = self.compiled_ranges.write().unwrap();
-            ranges.insert(addr..addr + (block.len as u64));
+            let (block, len) = self.jit_engine.compile_block(state);
 
-            Arc::new(block)
+            self.compiled_addrs.insert(addr, CacheRange::Start);
+            self.compiled_addrs.insert(addr + len, CacheRange::End);
+
+            block
         });
-        block.clone()
+        block
     }
 
     /// Check if an arbitrary address is cached
-    pub fn is_addr_compiled(&self, addr: u64) -> Option<u64> {
-        let ranges = self.compiled_ranges.read().unwrap();
-        ranges
-            .iter()
-            .find_map(|range| range.contains(&addr).then(|| range.start))
+    pub fn is_addr_compiled(&self, addr: usize) -> bool {
+        match self.compiled_addrs.range(..=addr).last() {
+            Some((_, CacheRange::Start)) => true,
+            _ => false,
+        }
     }
 }
 
@@ -45,7 +46,7 @@ impl Default for Cache {
         Self {
             jit_engine: JitEngine::new(),
             blocks: Default::default(),
-            compiled_ranges: Default::default(),
+            compiled_addrs: Default::default(),
         }
     }
 }
