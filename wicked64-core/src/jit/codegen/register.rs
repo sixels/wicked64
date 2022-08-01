9ba8cfb555ef71aa7aa1fd5d4aef07364d1abe49
Sixels <sixels@protonmail.com>
2022-08-01 22:57:06 +0000

    Fix register indexes and add `is_reserved` function to get the registers with special purposes

--- wicked64-core/src/jit/codegen/register.rs
+++ wicked64-core/src/jit/codegen/register.rs
@@ -1,25 +1,37 @@
+use num_enum::TryFromPrimitive;
+
 #[repr(u8)]
-#[derive(Clone, Copy)]
+#[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Debug, Hash, TryFromPrimitive)]
 pub enum X64Gpr {
-    Rax,
-    Rbx,
-    Rcx,
-    Rdx,
-    Rsi,
-    Rdi,
-    Rsp,
-    Rbp,
-    R8,
-    R9,
-    R10,
-    R11,
-    R12,
-    R13,
-    R14,
-    R15,
+    Rax = 0x0,
+    Rbx = 0x3,
+    Rcx = 0x1,
+    Rdx = 0x2,
+    Rsi = 0x6,
+    Rdi = 0x7,
+    Rsp = 0x4,
+    Rbp = 0x5,
+    R8 = 0x8,
+    R9 = 0x9,
+    R10 = 0xA,
+    R11 = 0xB,
+    R12 = 0xC,
+    R13 = 0xD,
+    R14 = 0xE,
+    R15 = 0xF,
 }
 
-pub const CALLEE_SAVED_REGISTERS: &[X64Gpr] = &[
+impl X64Gpr {
+    /// Check if register is one of: RSI, RDI, RSP, RBP
+    pub fn is_reserved(&self) -> bool {
+        match self {
+            Self::Rsi | Self::Rdi | Self::Rsp | Self::Rbp => true,
+            _ => false,
+        }
+    }
+}
+
+pub const CALLEE_SAVED_REGISTERS: [X64Gpr; 7] = [
     X64Gpr::Rbx,
     X64Gpr::Rsi,
     X64Gpr::Rbp,
