dfc08339f1db09b872c100c8837a9df4ebf65e0d
Sixels <sixels@protonmail.com>
2022-08-09 23:53:06 +0000

    Better register manager

--- wicked64-core/src/jit/register.rs
+++ wicked64-core/src/jit/register.rs
@@ -23,7 +23,7 @@ impl Hash for HostRegister {
 
 type RegMap = HashMap<GuestRegister, HostRegister>;
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct Registers {
     regs: RegMap,
     free_regs: HashSet<X64Gpr>,
@@ -44,9 +44,16 @@ impl Registers {
         }
     }
 
-    pub fn get_mapped_register<F>(&mut self, guest_reg: GuestRegister, drop_unused: F) -> X64Gpr
+    pub fn iter(&self) -> impl Iterator<Item = (GuestRegister, X64Gpr)> {
+        self.regs
+            .clone()
+            .into_iter()
+            .map(|(g, HostRegister(h, _))| (g, h))
+    }
+
+    pub fn get_mapped_register<F>(&mut self, guest_reg: GuestRegister, mut drop_unused: F) -> X64Gpr
     where
-        F: FnOnce(GuestRegister, X64Gpr),
+        F: FnMut(GuestRegister, X64Gpr) -> bool,
     {
         if let Some(HostRegister(host_reg, borrowed)) = self.regs.get_mut(&guest_reg) {
             *borrowed = self.borrow_count;
@@ -54,13 +61,27 @@ impl Registers {
             return *host_reg;
         }
 
-        let host_reg = if let Some(next) = self.free_regs.iter().next().clone() {
+        let host_reg = if let Some(next) = self.free_regs.iter().next() {
             // we still have free registers
             *next
         } else {
-            let (guest_reg, host_reg) = self.find_unused().unwrap();
-            drop_unused(guest_reg, host_reg);
-            host_reg
+            let mut unused_regs = self.regs.iter().collect::<Vec<_>>();
+            unused_regs.sort_by_key(|(_, HostRegister(_, n))| *n);
+
+            let find_unused = || {
+                for (&guest_reg, &HostRegister(host_reg, _)) in unused_regs.into_iter() {
+                    // We need to know if we can drop this register safely (We
+                    // don't want to drop tmp registers, as they are intended to
+                    // be manually dropped), so we need to ask before
+                    if drop_unused(guest_reg, host_reg) {
+                        self.free_regs.insert(host_reg);
+                        return Some(host_reg);
+                    }
+                }
+                None
+            };
+
+            find_unused().expect("Could not drop any register")
         };
 
         // At this point we already know the key does not exist, that's why we call `insert_unique_unchecked`
@@ -71,14 +92,30 @@ impl Registers {
         host_reg
     }
 
-    pub fn find_unused(&self) -> Option<(GuestRegister, X64Gpr)> {
+    pub fn find_host_register(&self, host_reg: X64Gpr) -> Option<(GuestRegister, X64Gpr)> {
         self.regs
             .iter()
-            .min_by_key(|(_, HostRegister(_, n))| *n)
-            .map(|(guest_reg, HostRegister(host_reg, _))| (*guest_reg, *host_reg))
+            .map(|(g, HostRegister(r, _))| (*g, *r))
+            .find(|(_, r)| *r == host_reg)
     }
 
-    pub fn drop_host_register(&mut self, reg: X64Gpr) {
-        self.free_regs.insert(reg);
+    pub fn drop_guest_register(&mut self, guest_reg: GuestRegister) {
+        self.regs
+            .remove(&guest_reg)
+            .map(|HostRegister(host_reg, _)| {
+                self.free_regs.insert(host_reg);
+            });
+
+        if self.regs.len() == 0 {
+            debug_assert_eq!(
+                self.free_regs.len(),
+                (0..16)
+                    .map(|r| X64Gpr::try_from(r).unwrap())
+                    .filter(|r| !r.is_reserved())
+                    .collect::<Vec<_>>()
+                    .len()
+            );
+            self.borrow_count = 0;
+        }
     }
 }
