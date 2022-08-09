0ecd835e96572bf153b72516812e4680296a3310
Sixels <sixels@protonmail.com>
2022-08-09 23:52:09 +0000

    Wrap State on EmulatorState

--- wicked64-core/src/jit/engine.rs
+++ wicked64-core/src/jit/engine.rs
@@ -1,10 +1,15 @@
 use std::cell::RefCell;
+use std::ops::{Deref, DerefMut};
 use std::rc::Rc;
 
 use crate::cpu::instruction::{ImmediateType, Instruction};
+use crate::jit::codegen::CallArg;
+use crate::mmu::MemoryUnit;
 use crate::n64::State;
 
 use super::code::{CompiledBlock, RawBlock};
+use super::codegen::callable::Callable;
+use super::codegen::register::CALLEE_SAVED_REGISTERS;
 use super::codegen::{register::X64Gpr, Emitter};
 use super::register::{GuestRegister, Registers};
 
@@ -27,9 +32,48 @@ impl JitEngine {
     }
 }
 
+struct EmulatorState(Rc<RefCell<State>>);
+
+impl EmulatorState {
+    pub fn new(state: Rc<RefCell<State>>) -> Self {
+        Self(state)
+    }
+
+    pub fn offset_of<F, T>(&self, get_offset: F) -> usize
+    where
+        F: FnOnce(&State) -> &T,
+    {
+        let state = self.0.borrow();
+
+        let data_addr = get_offset(&state) as *const T as usize;
+        let state_addr = self.state_ptr() as usize;
+
+        debug_assert!(state_addr <= data_addr);
+        data_addr - state_addr
+    }
+
+    pub fn state_ptr(&self) -> *const State {
+        &*self.0.borrow()
+    }
+}
+
+impl Deref for EmulatorState {
+    type Target = Rc<RefCell<State>>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl DerefMut for EmulatorState {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
+    }
+}
+
 /// The JIT compiler
 struct Jit {
-    state: Rc<RefCell<State>>,
+    state: EmulatorState,
     pc: u64,
     regs: Registers,
     code: RawBlock,
@@ -44,7 +88,7 @@ impl Jit {
 
         Self {
             pc,
-            state,
+            state: EmulatorState::new(state),
             regs: Registers::new(),
             code,
         }
@@ -80,7 +124,7 @@ impl Jit {
 
     fn compile_block(&mut self, cycles: usize) -> usize {
         // save the state address into `rsi` so we can easily access guest registers later
-        let state_addr = self.state_address() as *mut State as u64;
+        let state_addr = self.state.state_ptr() as *mut State as u64;
         self.code.emit_movabs_reg(X64Gpr::Rsi, state_addr).unwrap();
 
         let mut total_cycles = 0;
@@ -138,43 +182,96 @@ impl Jit {
 
                 // TODO: Implement `emit_or_reg_reg`
                 // or host_rs, tmp_reg
-                self.code
-                    .emit_raw(&[
-                        0x48,
-                        0x09,
-                        (0b11 << 6) | ((host_rs as u8) << 3) | ((tmp_reg as u8) << 0),
-                    ])
-                    .unwrap();
-
+                self.code.emit_or_reg_reg(tmp_reg, host_rs).unwrap();
                 self.code.emit_mov_reg_reg(host_rt, tmp_reg).unwrap();
 
-                self.regs.drop_host_register(tmp_reg);
+                self.regs.drop_guest_register(GuestRegister::Tmp(0));
 
                 false
+            }
+            Instruction::SW(ImmediateType {
+                rs: base,
+                rt,
+                immediate: offset,
+                ..
+            }) => {
+                // TODO: 64-bit offset support
+
+                fn mmu_store(state: *mut State, vaddr: usize, rt: u64) {
+                    let State { cpu, mmu } = unsafe { &mut *state };
+
+                    let addr = cpu.translate_virtual(vaddr);
+
+                    mmu.store::<_, byteorder::BigEndian>(addr, rt);
+                }
+
+                let vaddr_reg = self.get_tmp_register(0);
+                {
+                    let offset_ex = offset as i16 as usize;
+                    let base = self.host_reg_from_guest(GuestRegister::Cpu(base as usize));
+
+                    self.code
+                        .emit_mov_reg_immediate(vaddr_reg, offset_ex as u64)
+                        .unwrap();
+                    self.code.emit_add_reg_reg(vaddr_reg, base).unwrap();
+                }
+
+                self.sync_guest_register(X64Gpr::Rdx);
+                self.code.emit_push_reg(X64Gpr::Rsi).unwrap();
+                let rt = self.get_host_cpu_register(rt as usize);
+                self.code.emit_mov_reg_reg(X64Gpr::Rdx, rt).unwrap();
+                self.emit_call_wrapper(
+                    mmu_store as fn(_, _, _),
+                    &[
+                        CallArg::Val(self.state.state_ptr() as u64),
+                        CallArg::Reg(vaddr_reg),
+                    ],
+                    None,
+                );
+                self.code.emit_pop_reg(X64Gpr::Rsi).unwrap();
+
+                true
             }
             _ => todo!("Implement the rest of the instructions"),
         }
     }
 
-    fn state_address(&self) -> *const State {
-        &*self.state.borrow()
-    }
-    /// Get the relative address of a value inside the state
-    fn offset_of<T, F: FnOnce(&State) -> &T>(&self, rel_offset: F) -> usize {
-        let state = self.state.borrow();
+    fn emit_call_wrapper<const N: usize, I, O, C: Callable<N, I, O>>(
+        &mut self,
+        funct: C,
+        args: &[CallArg],
+        return_to: Option<X64Gpr>,
+    ) {
+        self.code.emit_push_reg(X64Gpr::Rsi).unwrap();
 
-        let data_addr = rel_offset(&state) as *const T as usize;
-        let state_addr = self.state_address() as usize;
+        let mut tmp_regs = Vec::new();
+        for (guest, host) in self.regs.iter() {
+            match guest {
+                GuestRegister::Tmp(_) => {
+                    if !CALLEE_SAVED_REGISTERS.contains(&host) {
+                        self.code.emit_push_reg(host).unwrap();
+                        tmp_regs.insert(0, host);
+                    }
+                }
+                _ => self.sync_guest_register(host),
+            }
+        }
 
-        debug_assert!(state_addr <= data_addr);
-        data_addr - state_addr
+        self.code.emit_call(funct, args).unwrap();
+        return_to.map(|ret| self.code.emit_mov_reg_reg(ret, X64Gpr::Rax));
+
+        for host in tmp_regs.into_iter() {
+            self.code.emit_pop_reg(host).unwrap();
+        }
+
+        self.code.emit_pop_reg(X64Gpr::Rsi).unwrap();
     }
 
     fn get_host_cpu_register(&mut self, register: usize) -> X64Gpr {
         let host_reg = self.host_reg_from_guest(GuestRegister::Cpu(register));
 
         // load the register value
-        let reg_offset = self.offset_of(|state| &state.cpu.gpr[register]);
+        let reg_offset = self.state.offset_of(|state| &state.cpu.gpr[register]);
         self.code
             .emit_mov_reg_qword_ptr(host_reg, reg_offset)
             .unwrap();
@@ -186,32 +283,40 @@ impl Jit {
     }
 
     /// Gets an unused host register from a guest one
-    pub fn host_reg_from_guest(&mut self, guest_reg: GuestRegister) -> X64Gpr {
-        let offset_of = {
-            let state = self.state.borrow();
-            let state_addr = &*state as *const _ as usize;
-
-            move |rel_offset: &dyn Fn(&State) -> &u64| -> i32 {
-                let data_addr = rel_offset(&state) as *const _ as usize;
-                debug_assert!(state_addr <= data_addr);
-
-                let offset = data_addr - state_addr;
-                debug_assert!(offset <= i32::MAX as _);
-                offset as i32
-            }
-        };
-
+    fn host_reg_from_guest(&mut self, guest_reg: GuestRegister) -> X64Gpr {
         self.regs
             .get_mapped_register(guest_reg, |old_guest_reg, host_reg| {
                 let old_guest_reg_offset = match old_guest_reg {
-                    GuestRegister::Cpu(reg) => offset_of(&|state| &state.cpu.gpr[reg]),
-                    GuestRegister::Cp0(reg) => offset_of(&|state| state.cpu.cp0.get_register(reg)),
-                    GuestRegister::Tmp(_) => return,
+                    GuestRegister::Cpu(reg) => self.state.offset_of(|state| &state.cpu.gpr[reg]),
+                    GuestRegister::Cp0(reg) => self
+                        .state
+                        .offset_of(|state| state.cpu.cp0.get_register(reg)),
+                    GuestRegister::Tmp(_) => return false,
                 };
 
                 self.code
-                    .emit_mov_rsi_rel_reg(old_guest_reg_offset, host_reg)
+                    .emit_mov_rsi_rel_reg(old_guest_reg_offset as i32, host_reg)
                     .unwrap();
+
+                true
             })
+    }
+
+    fn sync_guest_register(&mut self, reg: X64Gpr) {
+        if let Some((guest_reg, host_reg)) = self.regs.find_host_register(reg) {
+            let guest_offset = match guest_reg {
+                GuestRegister::Cpu(reg) => self.state.offset_of(|state| &state.cpu.gpr[reg]),
+                GuestRegister::Cp0(reg) => self
+                    .state
+                    .offset_of(|state| state.cpu.cp0.get_register(reg)),
+                GuestRegister::Tmp(_) => return,
+            };
+
+            debug_assert!(guest_offset <= i32::MAX as _);
+            self.code
+                .emit_mov_rsi_rel_reg(guest_offset as i32, host_reg)
+                .unwrap();
+            self.regs.drop_guest_register(guest_reg);
+        }
     }
 }
