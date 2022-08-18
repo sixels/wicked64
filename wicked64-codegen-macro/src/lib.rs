a31839c602b3c507fdb45c0b33fffb6441057ac5
Sixels <sixels@protonmail.com>
2022-08-18 23:13:08 +0000

    Create the proc_macro

---
+++ wicked64-codegen-macro/src/lib.rs
@@ -0,0 +1,44 @@
+mod emitter;
+mod instruction;
+mod register;
+mod addressing;
+
+use instruction::Instruction;
+use proc_macro::TokenStream;
+use quote::quote;
+use syn::{parse::Parse, parse_macro_input, Ident};
+
+struct Emit {
+    buffer: Ident,
+    instructions: Vec<Instruction>,
+}
+
+impl Parse for Emit {
+    fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
+        let buffer = input.parse()?;
+        let mut instructions = Vec::new();
+        while !input.is_empty() {
+            instructions.push(input.parse()?);
+        }
+        Ok(Self {
+            buffer,
+            instructions,
+        })
+    }
+}
+
+#[proc_macro]
+pub fn emit(tokens: TokenStream) -> TokenStream {
+    let Emit {
+        buffer,
+        instructions,
+    } = parse_macro_input!(tokens as Emit);
+
+    let mut src = quote! {
+      let buf = &mut #buffer;
+    };
+    let gen = instructions.into_iter().map(emitter::emit);
+    src.extend(gen);
+
+    src.into()
+}
