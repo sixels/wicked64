0d7e992588a83f17b0dcc4b28079502ab76a7bb8
Sixels <sixels@protonmail.com>
2022-08-21 21:09:08 +0000

    Fix Instruction display format error

--- wicked64-codegen-macro/src/instruction.rs
+++ wicked64-codegen-macro/src/instruction.rs
@@ -64,7 +64,7 @@ impl Display for Instruction {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         macro_rules! w {
             ($inst:literal) => {
-                write!(f, "{} {}", $inst)
+                write!(f, "{}", $inst)
             };
             ($inst:literal, $a:expr) => {
                 write!(f, "{} {}", $inst, $a)
