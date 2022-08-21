ee82c6d1789aa7e49440fad352cff911d66b1707
Sixels <sixels@protonmail.com>
2022-08-21 21:52:50 +0000

    Add more tests for mov instruction covering variable registers

--- wicked64-codegen/tests/mov.rs
+++ wicked64-codegen/tests/mov.rs
@@ -6,12 +6,16 @@ fn mov() {
     let mut emitter = Emitter::new();
 
     let rax = 0;
+    let r9 = 9;
+
     emit!(emitter,
         mov rcx, r8;
         mov r9, rax;
         mov rcx, rbx;
         mov r9, r11;
         mov r9, $rax;
+        mov $r9, rax;
+        mov $r9, $rax;
     );
 
     assert_eq!(
@@ -21,6 +25,8 @@ fn mov() {
             0x49, 0x89, 0xc1, // mov r9,rax
             0x48, 0x89, 0xd9, // mov rcx, rbx
             0x4d, 0x89, 0xd9, // mov r9, r11
+            0x49, 0x89, 0xc1, // mov r9,rax
+            0x49, 0x89, 0xc1, // mov r9,rax
             0x49, 0x89, 0xc1, // mov r9,rax
         ],
     );
