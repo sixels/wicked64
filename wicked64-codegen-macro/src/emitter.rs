be49e36d07d44e19a85dc6a8d8dae8dc1bf9c7e5
Sixels <sixels@protonmail.com>
2022-08-21 19:13:03 +0000

    Generate code for mov register

--- wicked64-codegen-macro/src/emitter.rs
+++ wicked64-codegen-macro/src/emitter.rs
@@ -1,6 +1,11 @@
 use proc_macro2::TokenStream;
+use quote::quote;
 
-use crate::{addressing::AddressingMode, instruction::Instruction, register::Register};
+use crate::{
+    addressing::{AddrIndirect, AddressingMode},
+    instruction::Instruction,
+    register::Register,
+};
 
 pub fn emit(instruction: Instruction) -> TokenStream {
     match instruction {
@@ -20,8 +25,9 @@ fn emit_mov(dst: AddressingMode, src: AddressingMode) -> TokenStream {
             let s = (src as u8) % 8;
             let d = (dst as u8) % 8;
             let mod_rm = (0b11 << 6) | (s << 3) | (d << 0);
-
-            // self.write(&[base, 0x89, mod_rm])?;
+            quote! {
+                buf.emit_raw!(&[#base, 0x89, #mod_rm])
+            }
         }
         (AddressingMode::Register(dst), AddressingMode::Immediate(im)) => {
             debug_assert!(dst != Register::Rsp);
@@ -30,11 +36,17 @@ fn emit_mov(dst: AddressingMode, src: AddressingMode) -> TokenStream {
             let base = dst_n + 0xb8;
             let im = im.0 as i32;
 
-            // if dst >= Register::R8 {
-            // self.write_u8(0x41)?;
-            // }
-            // self.write_u8(base)?;
-            // self.write_i32::<LittleEndian>(im)?;
+            let mut ts = TokenStream::new();
+            if dst >= Register::R8 {
+                ts.extend(quote! {
+                    buf.emit_byte(0x41);
+                })
+            }
+            ts.extend(quote! {
+                buf.emit_byte(#base);
+                buf.emit_dword(#im);
+            });
+            ts
         }
         (AddressingMode::Register(dst), AddressingMode::Direct(addr)) => {
             debug_assert!(dst != Register::Rsp);
@@ -43,30 +55,40 @@ fn emit_mov(dst: AddressingMode, src: AddressingMode) -> TokenStream {
             let d = (dst as u8) % 8;
             let mod_rm = (0b00 << 6) | (d << 3) | (0b100 << 0);
 
-            // self.write(&[base, 0x8b, mod_rm, 0x25])?;
-            // self.write_i32::<LittleEndian>(addr)?;
+            let addr = addr.addr;
+            quote! {
+                buf.emit_raw(&[#base, 0x8b, #mod_rm, 0x25]);
+                buf.emit_dword(#addr);
+            }
         }
         (AddressingMode::Register(dst), AddressingMode::Indirect(src)) => {
             debug_assert!(dst != Register::Rsp);
+            
+            let AddrIndirect { reg: src, disp, .. } = src;
             let base = (0b1001 << 3)
                 | (u8::from(dst >= Register::R8) << 2)
-                | (u8::from(src.reg >= Register::R8) << 0);
+                | (u8::from(src >= Register::R8) << 0);
 
-            let mode = u8::from(src.disp != 0) << 1;
-            let s = (src.reg as u8) % 8;
+            let mode = u8::from(disp != 0) << 1;
+            let s = (src as u8) % 8;
             let d = (dst as u8) % 8;
             let mod_rm = (mode << 6) | (d << 3) | (s << 0);
 
-            // self.write(&[base, 0x8b, mod_rm])?;
-            // if src == Register::Rsp {
-            //     self.write_u8(0x24)?;
-            // }
-            // if mode != 0 {
-            //     self.write_i32::<LittleEndian>(disp)?;
-            // }
+            let mut ts = quote! {
+                buf.emit_raw(&[#base, 0x8b, #mod_rm]);
+            };
+            if src == Register::Rsp {
+                ts.extend(quote! {
+                    buf.write_byte(0x24);
+                });
+            }
+            if mode != 0 {
+                ts.extend(quote! {
+                    buf.write_dword(#disp);
+                })
+            }
+            ts
         }
         _ => unimplemented!(),
     }
-
-    todo!()
 }
