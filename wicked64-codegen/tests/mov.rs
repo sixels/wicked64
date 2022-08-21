52896e7edc438b706df79d5d65a476d128932e36
Sixels <sixels@protonmail.com>
2022-08-21 22:28:37 +0000

    Add test to mov reg immediate instruction

--- wicked64-codegen/tests/mov.rs
+++ wicked64-codegen/tests/mov.rs
@@ -2,7 +2,7 @@ use wicked64_codegen::Emitter;
 use wicked64_codegen_macro::emit;
 
 #[test]
-fn mov() {
+fn mov_reg_reg() {
     let mut emitter = Emitter::new();
 
     let rax = 0;
@@ -20,7 +20,7 @@ fn mov() {
 
     assert_eq!(
         emitter.as_slice(),
-        vec![
+        &[
             0x4c, 0x89, 0xc1, // mov rcx, r8
             0x49, 0x89, 0xc1, // mov r9,rax
             0x48, 0x89, 0xd9, // mov rcx, rbx
@@ -30,4 +30,37 @@ fn mov() {
             0x49, 0x89, 0xc1, // mov r9,rax
         ],
     );
+}
+
+#[test]
+fn mov_reg_immediate() {
+    let mut emitter = Emitter::new();
+
+    let val = 0x3412;
+    let r11 = 11;
+
+    emit!(emitter,
+        mov rcx, 0x3412;
+        mov rbx, 0x3412;
+        mov r9, 0x3412;
+        mov r11, 0x3412;
+        mov rax, 0x3412;
+        mov r8, 0x3412;
+        mov $r11, 0x3412;
+        mov r11, $val;
+    );
+
+    assert_eq!(
+        emitter.as_slice(),
+        &[
+            0xb9, 0x12, 0x34, 0x00, 0x00, // mov rcx, 0x3412
+            0xbb, 0x12, 0x34, 0x00, 0x00, // mov rbx, 0x3412
+            0x41, 0xb9, 0x12, 0x34, 0x00, 0x00, // mov r9, 0x3412
+            0x41, 0xbb, 0x12, 0x34, 0x00, 0x00, // mov r11, 0x3412
+            0xb8, 0x12, 0x34, 0x00, 0x00, // mov rax, 0x3412
+            0x41, 0xb8, 0x12, 0x34, 0x00, 0x00, // mov r8, 0x3412
+            0x41, 0xbb, 0x12, 0x34, 0x00, 0x00, // mov r11, 0x3412
+            0x41, 0xbb, 0x12, 0x34, 0x00, 0x00, // mov r11, 0x3412
+        ]
+    )
 }
