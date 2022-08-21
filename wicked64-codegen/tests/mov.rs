ec4af3e14ff31017ce8bf425c189a34bcf0a9b7e
Sixels <sixels@protonmail.com>
2022-08-21 21:11:44 +0000

    Add tests for mov instructions [NOT PASSING YET]

---
+++ wicked64-codegen/tests/mov.rs
@@ -0,0 +1,27 @@
+use wicked64_codegen::Emitter;
+use wicked64_codegen_macro::emit;
+
+#[test]
+fn mov() {
+    let mut emitter = Emitter::new();
+
+    let rax = 0;
+    emit!(emitter,
+        mov rcx, r8;
+        mov r9, rax;
+        mov rcx, rbx;
+        mov r9, r11;
+        mov r9, $rax;
+    );
+
+    assert_eq!(
+        emitter.as_slice(),
+        vec![
+            0x4c, 0x89, 0xc1, // mov rcx, r8
+            0x49, 0x89, 0xc1, // mov r9,rax
+            0x48, 0x89, 0xd9, // mov rcx, rbx
+            0x4d, 0x89, 0xd9, // mov r9, r11
+            0x49, 0x89, 0xc1, // mov r9,rax
+        ],
+    );
+}
