9a06fd7b348e0f043959eb0c7cf53e2acc1f4665
Sixels <sixels@protonmail.com>
2022-09-04 19:24:59 +0000

    Add mod `register` and update re-exports

--- wicked64-codegen/lib/src/lib.rs
+++ wicked64-codegen/lib/src/lib.rs
@@ -1,9 +1,9 @@
 mod emitter;
-
-pub use emitter::Emitter;
+pub mod register;
 
 pub use w64_codegen_macro::emit;
-pub use w64_codegen_types::register;
+
+pub use emitter::Emitter;
 
 pub mod prelude {
     pub use super::register::Register;
