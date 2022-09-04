0a0edb0d09ebb46d4cddce05425e9a609afdc787
Sixels <sixels@protonmail.com>
2022-09-04 19:17:07 +0000

    Add helpers for type checking and code generation inside the main macro

--- wicked64-codegen/macro/src/lib.rs
+++ wicked64-codegen/macro/src/lib.rs
@@ -45,10 +45,23 @@ pub fn emit(tokens: TokenStream) -> TokenStream {
 
     let mut src = quote! {
         use w64_codegen::prelude::*;
+        use w64_codegen::macro_internals::_emit_instructions;
+
+        fn assert_sized<T: Sized>(_: &T) {}
+
         let buf = &mut #buffer;
     };
-    let gen = instructions.into_iter().map(emitter::emit);
-    src.extend(gen);
+    src.extend(instructions.0.into_iter().map(emitter::emit));
 
     quote!({ #src }).into()
+}
+
+#[proc_macro]
+pub fn _emit_instructions(tokens: TokenStream) -> TokenStream {
+    let instructions = parse_macro_input!(tokens as Instructions);
+    (instructions.0)
+        .into_iter()
+        .map(emitter::emit)
+        .collect::<proc_macro2::TokenStream>()
+        .into()
 }
