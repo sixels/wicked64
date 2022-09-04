187445d4dd8239cf320980f814f76a2d1d34f5c5
Sixels <sixels@protonmail.com>
2022-09-04 19:26:31 +0000

    Update Emitter and add ExecBuffer to store the generated code in an executable memory region

--- wicked64-codegen/lib/src/emitter.rs
+++ wicked64-codegen/lib/src/emitter.rs
@@ -1,3 +1,6 @@
+use crate::{self as w64_codegen, register::CALLEE_SAVED_REGISTERS};
+
+use crate::emit;
 use byteorder::{LittleEndian, WriteBytesExt};
 
 pub struct Emitter {
@@ -6,10 +9,43 @@ pub struct Emitter {
 
 impl Emitter {
     pub fn new() -> Self {
-        Self { buffer: Vec::new() }
+        let mut emitter = Self::default();
+
+        emit!(emitter,
+            push rax;
+        );
+        for reg in CALLEE_SAVED_REGISTERS {
+            emit!(emitter,
+                push %reg;
+            );
+        }
+
+        emitter
     }
+
     pub fn as_slice(&self) -> &[u8] {
         &self.buffer
+    }
+
+    pub fn finalize(self) -> region::Result<ExecBuffer> {
+        let mut emitter = self;
+
+        // retrieve callee-saved registers
+        for reg in CALLEE_SAVED_REGISTERS.iter().copied().rev() {
+            emit!(emitter,
+                pop %reg;
+            );
+        }
+        emit!(emitter,
+            pop rcx;
+            ret;
+        );
+
+        unsafe { emitter.make_exec() }
+    }
+
+    pub unsafe fn make_exec(self) -> region::Result<ExecBuffer> {
+        ExecBuffer::new(self.buffer)
     }
 
     pub fn emit_raw(&mut self, raw_bytes: &[u8]) {
@@ -26,5 +62,36 @@ impl Emitter {
     }
     pub fn emit_qword(&mut self, qword: u64) {
         self.buffer.write_u64::<LittleEndian>(qword).unwrap();
+    }
+}
+
+impl Default for Emitter {
+    fn default() -> Self {
+        Self { buffer: Vec::new() }
+    }
+}
+pub struct ExecBuffer {
+    ptr: *const u8,
+    buf: Vec<u8>,
+}
+
+impl ExecBuffer {
+    unsafe fn new(buffer: Vec<u8>) -> region::Result<Self> {
+        let ptr = buffer.as_ptr();
+
+        region::protect(ptr, buffer.len(), region::Protection::READ_WRITE_EXECUTE)?;
+
+        Ok(Self { buf: buffer, ptr })
+    }
+
+    pub fn execute(self) {
+        unsafe {
+            let f: unsafe extern "C" fn() = std::mem::transmute(self.ptr);
+            f();
+        }
+    }
+
+    pub fn as_slice(&self) -> &[u8] {
+        self.buf.as_slice()
     }
 }
