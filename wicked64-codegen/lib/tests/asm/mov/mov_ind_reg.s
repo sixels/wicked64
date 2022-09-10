ed7ccb26cc5b1bceccab6b9cb26402bb9af74c6f
Sixels <sixels@protonmail.com>
2022-09-10 22:44:18 +0000

    Change asm tests structure

---
+++ wicked64-codegen/lib/tests/asm/mov/mov_ind_reg.s
@@ -0,0 +1,768 @@
+mov [rax], rax
+mov [rax + 0x123456], rax
+mov [rax - 0x70], rax
+mov [rax], rcx
+mov [rax + 0x123456], rcx
+mov [rax - 0x70], rcx
+mov [rax], rdx
+mov [rax + 0x123456], rdx
+mov [rax - 0x70], rdx
+mov [rax], rbx
+mov [rax + 0x123456], rbx
+mov [rax - 0x70], rbx
+mov [rax], rsp
+mov [rax + 0x123456], rsp
+mov [rax - 0x70], rsp
+mov [rax], rbp
+mov [rax + 0x123456], rbp
+mov [rax - 0x70], rbp
+mov [rax], rsi
+mov [rax + 0x123456], rsi
+mov [rax - 0x70], rsi
+mov [rax], rdi
+mov [rax + 0x123456], rdi
+mov [rax - 0x70], rdi
+mov [rax], r8
+mov [rax + 0x123456], r8
+mov [rax - 0x70], r8
+mov [rax], r9
+mov [rax + 0x123456], r9
+mov [rax - 0x70], r9
+mov [rax], r10
+mov [rax + 0x123456], r10
+mov [rax - 0x70], r10
+mov [rax], r11
+mov [rax + 0x123456], r11
+mov [rax - 0x70], r11
+mov [rax], r12
+mov [rax + 0x123456], r12
+mov [rax - 0x70], r12
+mov [rax], r13
+mov [rax + 0x123456], r13
+mov [rax - 0x70], r13
+mov [rax], r14
+mov [rax + 0x123456], r14
+mov [rax - 0x70], r14
+mov [rax], r15
+mov [rax + 0x123456], r15
+mov [rax - 0x70], r15
+mov [rcx], rax
+mov [rcx + 0x123456], rax
+mov [rcx - 0x70], rax
+mov [rcx], rcx
+mov [rcx + 0x123456], rcx
+mov [rcx - 0x70], rcx
+mov [rcx], rdx
+mov [rcx + 0x123456], rdx
+mov [rcx - 0x70], rdx
+mov [rcx], rbx
+mov [rcx + 0x123456], rbx
+mov [rcx - 0x70], rbx
+mov [rcx], rsp
+mov [rcx + 0x123456], rsp
+mov [rcx - 0x70], rsp
+mov [rcx], rbp
+mov [rcx + 0x123456], rbp
+mov [rcx - 0x70], rbp
+mov [rcx], rsi
+mov [rcx + 0x123456], rsi
+mov [rcx - 0x70], rsi
+mov [rcx], rdi
+mov [rcx + 0x123456], rdi
+mov [rcx - 0x70], rdi
+mov [rcx], r8
+mov [rcx + 0x123456], r8
+mov [rcx - 0x70], r8
+mov [rcx], r9
+mov [rcx + 0x123456], r9
+mov [rcx - 0x70], r9
+mov [rcx], r10
+mov [rcx + 0x123456], r10
+mov [rcx - 0x70], r10
+mov [rcx], r11
+mov [rcx + 0x123456], r11
+mov [rcx - 0x70], r11
+mov [rcx], r12
+mov [rcx + 0x123456], r12
+mov [rcx - 0x70], r12
+mov [rcx], r13
+mov [rcx + 0x123456], r13
+mov [rcx - 0x70], r13
+mov [rcx], r14
+mov [rcx + 0x123456], r14
+mov [rcx - 0x70], r14
+mov [rcx], r15
+mov [rcx + 0x123456], r15
+mov [rcx - 0x70], r15
+mov [rdx], rax
+mov [rdx + 0x123456], rax
+mov [rdx - 0x70], rax
+mov [rdx], rcx
+mov [rdx + 0x123456], rcx
+mov [rdx - 0x70], rcx
+mov [rdx], rdx
+mov [rdx + 0x123456], rdx
+mov [rdx - 0x70], rdx
+mov [rdx], rbx
+mov [rdx + 0x123456], rbx
+mov [rdx - 0x70], rbx
+mov [rdx], rsp
+mov [rdx + 0x123456], rsp
+mov [rdx - 0x70], rsp
+mov [rdx], rbp
+mov [rdx + 0x123456], rbp
+mov [rdx - 0x70], rbp
+mov [rdx], rsi
+mov [rdx + 0x123456], rsi
+mov [rdx - 0x70], rsi
+mov [rdx], rdi
+mov [rdx + 0x123456], rdi
+mov [rdx - 0x70], rdi
+mov [rdx], r8
+mov [rdx + 0x123456], r8
+mov [rdx - 0x70], r8
+mov [rdx], r9
+mov [rdx + 0x123456], r9
+mov [rdx - 0x70], r9
+mov [rdx], r10
+mov [rdx + 0x123456], r10
+mov [rdx - 0x70], r10
+mov [rdx], r11
+mov [rdx + 0x123456], r11
+mov [rdx - 0x70], r11
+mov [rdx], r12
+mov [rdx + 0x123456], r12
+mov [rdx - 0x70], r12
+mov [rdx], r13
+mov [rdx + 0x123456], r13
+mov [rdx - 0x70], r13
+mov [rdx], r14
+mov [rdx + 0x123456], r14
+mov [rdx - 0x70], r14
+mov [rdx], r15
+mov [rdx + 0x123456], r15
+mov [rdx - 0x70], r15
+mov [rbx], rax
+mov [rbx + 0x123456], rax
+mov [rbx - 0x70], rax
+mov [rbx], rcx
+mov [rbx + 0x123456], rcx
+mov [rbx - 0x70], rcx
+mov [rbx], rdx
+mov [rbx + 0x123456], rdx
+mov [rbx - 0x70], rdx
+mov [rbx], rbx
+mov [rbx + 0x123456], rbx
+mov [rbx - 0x70], rbx
+mov [rbx], rsp
+mov [rbx + 0x123456], rsp
+mov [rbx - 0x70], rsp
+mov [rbx], rbp
+mov [rbx + 0x123456], rbp
+mov [rbx - 0x70], rbp
+mov [rbx], rsi
+mov [rbx + 0x123456], rsi
+mov [rbx - 0x70], rsi
+mov [rbx], rdi
+mov [rbx + 0x123456], rdi
+mov [rbx - 0x70], rdi
+mov [rbx], r8
+mov [rbx + 0x123456], r8
+mov [rbx - 0x70], r8
+mov [rbx], r9
+mov [rbx + 0x123456], r9
+mov [rbx - 0x70], r9
+mov [rbx], r10
+mov [rbx + 0x123456], r10
+mov [rbx - 0x70], r10
+mov [rbx], r11
+mov [rbx + 0x123456], r11
+mov [rbx - 0x70], r11
+mov [rbx], r12
+mov [rbx + 0x123456], r12
+mov [rbx - 0x70], r12
+mov [rbx], r13
+mov [rbx + 0x123456], r13
+mov [rbx - 0x70], r13
+mov [rbx], r14
+mov [rbx + 0x123456], r14
+mov [rbx - 0x70], r14
+mov [rbx], r15
+mov [rbx + 0x123456], r15
+mov [rbx - 0x70], r15
+mov [rsp], rax
+mov [rsp + 0x123456], rax
+mov [rsp - 0x70], rax
+mov [rsp], rcx
+mov [rsp + 0x123456], rcx
+mov [rsp - 0x70], rcx
+mov [rsp], rdx
+mov [rsp + 0x123456], rdx
+mov [rsp - 0x70], rdx
+mov [rsp], rbx
+mov [rsp + 0x123456], rbx
+mov [rsp - 0x70], rbx
+mov [rsp], rsp
+mov [rsp + 0x123456], rsp
+mov [rsp - 0x70], rsp
+mov [rsp], rbp
+mov [rsp + 0x123456], rbp
+mov [rsp - 0x70], rbp
+mov [rsp], rsi
+mov [rsp + 0x123456], rsi
+mov [rsp - 0x70], rsi
+mov [rsp], rdi
+mov [rsp + 0x123456], rdi
+mov [rsp - 0x70], rdi
+mov [rsp], r8
+mov [rsp + 0x123456], r8
+mov [rsp - 0x70], r8
+mov [rsp], r9
+mov [rsp + 0x123456], r9
+mov [rsp - 0x70], r9
+mov [rsp], r10
+mov [rsp + 0x123456], r10
+mov [rsp - 0x70], r10
+mov [rsp], r11
+mov [rsp + 0x123456], r11
+mov [rsp - 0x70], r11
+mov [rsp], r12
+mov [rsp + 0x123456], r12
+mov [rsp - 0x70], r12
+mov [rsp], r13
+mov [rsp + 0x123456], r13
+mov [rsp - 0x70], r13
+mov [rsp], r14
+mov [rsp + 0x123456], r14
+mov [rsp - 0x70], r14
+mov [rsp], r15
+mov [rsp + 0x123456], r15
+mov [rsp - 0x70], r15
+mov [rbp], rax
+mov [rbp + 0x123456], rax
+mov [rbp - 0x70], rax
+mov [rbp], rcx
+mov [rbp + 0x123456], rcx
+mov [rbp - 0x70], rcx
+mov [rbp], rdx
+mov [rbp + 0x123456], rdx
+mov [rbp - 0x70], rdx
+mov [rbp], rbx
+mov [rbp + 0x123456], rbx
+mov [rbp - 0x70], rbx
+mov [rbp], rsp
+mov [rbp + 0x123456], rsp
+mov [rbp - 0x70], rsp
+mov [rbp], rbp
+mov [rbp + 0x123456], rbp
+mov [rbp - 0x70], rbp
+mov [rbp], rsi
+mov [rbp + 0x123456], rsi
+mov [rbp - 0x70], rsi
+mov [rbp], rdi
+mov [rbp + 0x123456], rdi
+mov [rbp - 0x70], rdi
+mov [rbp], r8
+mov [rbp + 0x123456], r8
+mov [rbp - 0x70], r8
+mov [rbp], r9
+mov [rbp + 0x123456], r9
+mov [rbp - 0x70], r9
+mov [rbp], r10
+mov [rbp + 0x123456], r10
+mov [rbp - 0x70], r10
+mov [rbp], r11
+mov [rbp + 0x123456], r11
+mov [rbp - 0x70], r11
+mov [rbp], r12
+mov [rbp + 0x123456], r12
+mov [rbp - 0x70], r12
+mov [rbp], r13
+mov [rbp + 0x123456], r13
+mov [rbp - 0x70], r13
+mov [rbp], r14
+mov [rbp + 0x123456], r14
+mov [rbp - 0x70], r14
+mov [rbp], r15
+mov [rbp + 0x123456], r15
+mov [rbp - 0x70], r15
+mov [rsi], rax
+mov [rsi + 0x123456], rax
+mov [rsi - 0x70], rax
+mov [rsi], rcx
+mov [rsi + 0x123456], rcx
+mov [rsi - 0x70], rcx
+mov [rsi], rdx
+mov [rsi + 0x123456], rdx
+mov [rsi - 0x70], rdx
+mov [rsi], rbx
+mov [rsi + 0x123456], rbx
+mov [rsi - 0x70], rbx
+mov [rsi], rsp
+mov [rsi + 0x123456], rsp
+mov [rsi - 0x70], rsp
+mov [rsi], rbp
+mov [rsi + 0x123456], rbp
+mov [rsi - 0x70], rbp
+mov [rsi], rsi
+mov [rsi + 0x123456], rsi
+mov [rsi - 0x70], rsi
+mov [rsi], rdi
+mov [rsi + 0x123456], rdi
+mov [rsi - 0x70], rdi
+mov [rsi], r8
+mov [rsi + 0x123456], r8
+mov [rsi - 0x70], r8
+mov [rsi], r9
+mov [rsi + 0x123456], r9
+mov [rsi - 0x70], r9
+mov [rsi], r10
+mov [rsi + 0x123456], r10
+mov [rsi - 0x70], r10
+mov [rsi], r11
+mov [rsi + 0x123456], r11
+mov [rsi - 0x70], r11
+mov [rsi], r12
+mov [rsi + 0x123456], r12
+mov [rsi - 0x70], r12
+mov [rsi], r13
+mov [rsi + 0x123456], r13
+mov [rsi - 0x70], r13
+mov [rsi], r14
+mov [rsi + 0x123456], r14
+mov [rsi - 0x70], r14
+mov [rsi], r15
+mov [rsi + 0x123456], r15
+mov [rsi - 0x70], r15
+mov [rdi], rax
+mov [rdi + 0x123456], rax
+mov [rdi - 0x70], rax
+mov [rdi], rcx
+mov [rdi + 0x123456], rcx
+mov [rdi - 0x70], rcx
+mov [rdi], rdx
+mov [rdi + 0x123456], rdx
+mov [rdi - 0x70], rdx
+mov [rdi], rbx
+mov [rdi + 0x123456], rbx
+mov [rdi - 0x70], rbx
+mov [rdi], rsp
+mov [rdi + 0x123456], rsp
+mov [rdi - 0x70], rsp
+mov [rdi], rbp
+mov [rdi + 0x123456], rbp
+mov [rdi - 0x70], rbp
+mov [rdi], rsi
+mov [rdi + 0x123456], rsi
+mov [rdi - 0x70], rsi
+mov [rdi], rdi
+mov [rdi + 0x123456], rdi
+mov [rdi - 0x70], rdi
+mov [rdi], r8
+mov [rdi + 0x123456], r8
+mov [rdi - 0x70], r8
+mov [rdi], r9
+mov [rdi + 0x123456], r9
+mov [rdi - 0x70], r9
+mov [rdi], r10
+mov [rdi + 0x123456], r10
+mov [rdi - 0x70], r10
+mov [rdi], r11
+mov [rdi + 0x123456], r11
+mov [rdi - 0x70], r11
+mov [rdi], r12
+mov [rdi + 0x123456], r12
+mov [rdi - 0x70], r12
+mov [rdi], r13
+mov [rdi + 0x123456], r13
+mov [rdi - 0x70], r13
+mov [rdi], r14
+mov [rdi + 0x123456], r14
+mov [rdi - 0x70], r14
+mov [rdi], r15
+mov [rdi + 0x123456], r15
+mov [rdi - 0x70], r15
+mov [r8], rax
+mov [r8 + 0x123456], rax
+mov [r8 - 0x70], rax
+mov [r8], rcx
+mov [r8 + 0x123456], rcx
+mov [r8 - 0x70], rcx
+mov [r8], rdx
+mov [r8 + 0x123456], rdx
+mov [r8 - 0x70], rdx
+mov [r8], rbx
+mov [r8 + 0x123456], rbx
+mov [r8 - 0x70], rbx
+mov [r8], rsp
+mov [r8 + 0x123456], rsp
+mov [r8 - 0x70], rsp
+mov [r8], rbp
+mov [r8 + 0x123456], rbp
+mov [r8 - 0x70], rbp
+mov [r8], rsi
+mov [r8 + 0x123456], rsi
+mov [r8 - 0x70], rsi
+mov [r8], rdi
+mov [r8 + 0x123456], rdi
+mov [r8 - 0x70], rdi
+mov [r8], r8
+mov [r8 + 0x123456], r8
+mov [r8 - 0x70], r8
+mov [r8], r9
+mov [r8 + 0x123456], r9
+mov [r8 - 0x70], r9
+mov [r8], r10
+mov [r8 + 0x123456], r10
+mov [r8 - 0x70], r10
+mov [r8], r11
+mov [r8 + 0x123456], r11
+mov [r8 - 0x70], r11
+mov [r8], r12
+mov [r8 + 0x123456], r12
+mov [r8 - 0x70], r12
+mov [r8], r13
+mov [r8 + 0x123456], r13
+mov [r8 - 0x70], r13
+mov [r8], r14
+mov [r8 + 0x123456], r14
+mov [r8 - 0x70], r14
+mov [r8], r15
+mov [r8 + 0x123456], r15
+mov [r8 - 0x70], r15
+mov [r9], rax
+mov [r9 + 0x123456], rax
+mov [r9 - 0x70], rax
+mov [r9], rcx
+mov [r9 + 0x123456], rcx
+mov [r9 - 0x70], rcx
+mov [r9], rdx
+mov [r9 + 0x123456], rdx
+mov [r9 - 0x70], rdx
+mov [r9], rbx
+mov [r9 + 0x123456], rbx
+mov [r9 - 0x70], rbx
+mov [r9], rsp
+mov [r9 + 0x123456], rsp
+mov [r9 - 0x70], rsp
+mov [r9], rbp
+mov [r9 + 0x123456], rbp
+mov [r9 - 0x70], rbp
+mov [r9], rsi
+mov [r9 + 0x123456], rsi
+mov [r9 - 0x70], rsi
+mov [r9], rdi
+mov [r9 + 0x123456], rdi
+mov [r9 - 0x70], rdi
+mov [r9], r8
+mov [r9 + 0x123456], r8
+mov [r9 - 0x70], r8
+mov [r9], r9
+mov [r9 + 0x123456], r9
+mov [r9 - 0x70], r9
+mov [r9], r10
+mov [r9 + 0x123456], r10
+mov [r9 - 0x70], r10
+mov [r9], r11
+mov [r9 + 0x123456], r11
+mov [r9 - 0x70], r11
+mov [r9], r12
+mov [r9 + 0x123456], r12
+mov [r9 - 0x70], r12
+mov [r9], r13
+mov [r9 + 0x123456], r13
+mov [r9 - 0x70], r13
+mov [r9], r14
+mov [r9 + 0x123456], r14
+mov [r9 - 0x70], r14
+mov [r9], r15
+mov [r9 + 0x123456], r15
+mov [r9 - 0x70], r15
+mov [r10], rax
+mov [r10 + 0x123456], rax
+mov [r10 - 0x70], rax
+mov [r10], rcx
+mov [r10 + 0x123456], rcx
+mov [r10 - 0x70], rcx
+mov [r10], rdx
+mov [r10 + 0x123456], rdx
+mov [r10 - 0x70], rdx
+mov [r10], rbx
+mov [r10 + 0x123456], rbx
+mov [r10 - 0x70], rbx
+mov [r10], rsp
+mov [r10 + 0x123456], rsp
+mov [r10 - 0x70], rsp
+mov [r10], rbp
+mov [r10 + 0x123456], rbp
+mov [r10 - 0x70], rbp
+mov [r10], rsi
+mov [r10 + 0x123456], rsi
+mov [r10 - 0x70], rsi
+mov [r10], rdi
+mov [r10 + 0x123456], rdi
+mov [r10 - 0x70], rdi
+mov [r10], r8
+mov [r10 + 0x123456], r8
+mov [r10 - 0x70], r8
+mov [r10], r9
+mov [r10 + 0x123456], r9
+mov [r10 - 0x70], r9
+mov [r10], r10
+mov [r10 + 0x123456], r10
+mov [r10 - 0x70], r10
+mov [r10], r11
+mov [r10 + 0x123456], r11
+mov [r10 - 0x70], r11
+mov [r10], r12
+mov [r10 + 0x123456], r12
+mov [r10 - 0x70], r12
+mov [r10], r13
+mov [r10 + 0x123456], r13
+mov [r10 - 0x70], r13
+mov [r10], r14
+mov [r10 + 0x123456], r14
+mov [r10 - 0x70], r14
+mov [r10], r15
+mov [r10 + 0x123456], r15
+mov [r10 - 0x70], r15
+mov [r11], rax
+mov [r11 + 0x123456], rax
+mov [r11 - 0x70], rax
+mov [r11], rcx
+mov [r11 + 0x123456], rcx
+mov [r11 - 0x70], rcx
+mov [r11], rdx
+mov [r11 + 0x123456], rdx
+mov [r11 - 0x70], rdx
+mov [r11], rbx
+mov [r11 + 0x123456], rbx
+mov [r11 - 0x70], rbx
+mov [r11], rsp
+mov [r11 + 0x123456], rsp
+mov [r11 - 0x70], rsp
+mov [r11], rbp
+mov [r11 + 0x123456], rbp
+mov [r11 - 0x70], rbp
+mov [r11], rsi
+mov [r11 + 0x123456], rsi
+mov [r11 - 0x70], rsi
+mov [r11], rdi
+mov [r11 + 0x123456], rdi
+mov [r11 - 0x70], rdi
+mov [r11], r8
+mov [r11 + 0x123456], r8
+mov [r11 - 0x70], r8
+mov [r11], r9
+mov [r11 + 0x123456], r9
+mov [r11 - 0x70], r9
+mov [r11], r10
+mov [r11 + 0x123456], r10
+mov [r11 - 0x70], r10
+mov [r11], r11
+mov [r11 + 0x123456], r11
+mov [r11 - 0x70], r11
+mov [r11], r12
+mov [r11 + 0x123456], r12
+mov [r11 - 0x70], r12
+mov [r11], r13
+mov [r11 + 0x123456], r13
+mov [r11 - 0x70], r13
+mov [r11], r14
+mov [r11 + 0x123456], r14
+mov [r11 - 0x70], r14
+mov [r11], r15
+mov [r11 + 0x123456], r15
+mov [r11 - 0x70], r15
+mov [r12], rax
+mov [r12 + 0x123456], rax
+mov [r12 - 0x70], rax
+mov [r12], rcx
+mov [r12 + 0x123456], rcx
+mov [r12 - 0x70], rcx
+mov [r12], rdx
+mov [r12 + 0x123456], rdx
+mov [r12 - 0x70], rdx
+mov [r12], rbx
+mov [r12 + 0x123456], rbx
+mov [r12 - 0x70], rbx
+mov [r12], rsp
+mov [r12 + 0x123456], rsp
+mov [r12 - 0x70], rsp
+mov [r12], rbp
+mov [r12 + 0x123456], rbp
+mov [r12 - 0x70], rbp
+mov [r12], rsi
+mov [r12 + 0x123456], rsi
+mov [r12 - 0x70], rsi
+mov [r12], rdi
+mov [r12 + 0x123456], rdi
+mov [r12 - 0x70], rdi
+mov [r12], r8
+mov [r12 + 0x123456], r8
+mov [r12 - 0x70], r8
+mov [r12], r9
+mov [r12 + 0x123456], r9
+mov [r12 - 0x70], r9
+mov [r12], r10
+mov [r12 + 0x123456], r10
+mov [r12 - 0x70], r10
+mov [r12], r11
+mov [r12 + 0x123456], r11
+mov [r12 - 0x70], r11
+mov [r12], r12
+mov [r12 + 0x123456], r12
+mov [r12 - 0x70], r12
+mov [r12], r13
+mov [r12 + 0x123456], r13
+mov [r12 - 0x70], r13
+mov [r12], r14
+mov [r12 + 0x123456], r14
+mov [r12 - 0x70], r14
+mov [r12], r15
+mov [r12 + 0x123456], r15
+mov [r12 - 0x70], r15
+mov [r13], rax
+mov [r13 + 0x123456], rax
+mov [r13 - 0x70], rax
+mov [r13], rcx
+mov [r13 + 0x123456], rcx
+mov [r13 - 0x70], rcx
+mov [r13], rdx
+mov [r13 + 0x123456], rdx
+mov [r13 - 0x70], rdx
+mov [r13], rbx
+mov [r13 + 0x123456], rbx
+mov [r13 - 0x70], rbx
+mov [r13], rsp
+mov [r13 + 0x123456], rsp
+mov [r13 - 0x70], rsp
+mov [r13], rbp
+mov [r13 + 0x123456], rbp
+mov [r13 - 0x70], rbp
+mov [r13], rsi
+mov [r13 + 0x123456], rsi
+mov [r13 - 0x70], rsi
+mov [r13], rdi
+mov [r13 + 0x123456], rdi
+mov [r13 - 0x70], rdi
+mov [r13], r8
+mov [r13 + 0x123456], r8
+mov [r13 - 0x70], r8
+mov [r13], r9
+mov [r13 + 0x123456], r9
+mov [r13 - 0x70], r9
+mov [r13], r10
+mov [r13 + 0x123456], r10
+mov [r13 - 0x70], r10
+mov [r13], r11
+mov [r13 + 0x123456], r11
+mov [r13 - 0x70], r11
+mov [r13], r12
+mov [r13 + 0x123456], r12
+mov [r13 - 0x70], r12
+mov [r13], r13
+mov [r13 + 0x123456], r13
+mov [r13 - 0x70], r13
+mov [r13], r14
+mov [r13 + 0x123456], r14
+mov [r13 - 0x70], r14
+mov [r13], r15
+mov [r13 + 0x123456], r15
+mov [r13 - 0x70], r15
+mov [r14], rax
+mov [r14 + 0x123456], rax
+mov [r14 - 0x70], rax
+mov [r14], rcx
+mov [r14 + 0x123456], rcx
+mov [r14 - 0x70], rcx
+mov [r14], rdx
+mov [r14 + 0x123456], rdx
+mov [r14 - 0x70], rdx
+mov [r14], rbx
+mov [r14 + 0x123456], rbx
+mov [r14 - 0x70], rbx
+mov [r14], rsp
+mov [r14 + 0x123456], rsp
+mov [r14 - 0x70], rsp
+mov [r14], rbp
+mov [r14 + 0x123456], rbp
+mov [r14 - 0x70], rbp
+mov [r14], rsi
+mov [r14 + 0x123456], rsi
+mov [r14 - 0x70], rsi
+mov [r14], rdi
+mov [r14 + 0x123456], rdi
+mov [r14 - 0x70], rdi
+mov [r14], r8
+mov [r14 + 0x123456], r8
+mov [r14 - 0x70], r8
+mov [r14], r9
+mov [r14 + 0x123456], r9
+mov [r14 - 0x70], r9
+mov [r14], r10
+mov [r14 + 0x123456], r10
+mov [r14 - 0x70], r10
+mov [r14], r11
+mov [r14 + 0x123456], r11
+mov [r14 - 0x70], r11
+mov [r14], r12
+mov [r14 + 0x123456], r12
+mov [r14 - 0x70], r12
+mov [r14], r13
+mov [r14 + 0x123456], r13
+mov [r14 - 0x70], r13
+mov [r14], r14
+mov [r14 + 0x123456], r14
+mov [r14 - 0x70], r14
+mov [r14], r15
+mov [r14 + 0x123456], r15
+mov [r14 - 0x70], r15
+mov [r15], rax
+mov [r15 + 0x123456], rax
+mov [r15 - 0x70], rax
+mov [r15], rcx
+mov [r15 + 0x123456], rcx
+mov [r15 - 0x70], rcx
+mov [r15], rdx
+mov [r15 + 0x123456], rdx
+mov [r15 - 0x70], rdx
+mov [r15], rbx
+mov [r15 + 0x123456], rbx
+mov [r15 - 0x70], rbx
+mov [r15], rsp
+mov [r15 + 0x123456], rsp
+mov [r15 - 0x70], rsp
+mov [r15], rbp
+mov [r15 + 0x123456], rbp
+mov [r15 - 0x70], rbp
+mov [r15], rsi
+mov [r15 + 0x123456], rsi
+mov [r15 - 0x70], rsi
+mov [r15], rdi
+mov [r15 + 0x123456], rdi
+mov [r15 - 0x70], rdi
+mov [r15], r8
+mov [r15 + 0x123456], r8
+mov [r15 - 0x70], r8
+mov [r15], r9
+mov [r15 + 0x123456], r9
+mov [r15 - 0x70], r9
+mov [r15], r10
+mov [r15 + 0x123456], r10
+mov [r15 - 0x70], r10
+mov [r15], r11
+mov [r15 + 0x123456], r11
+mov [r15 - 0x70], r11
+mov [r15], r12
+mov [r15 + 0x123456], r12
+mov [r15 - 0x70], r12
+mov [r15], r13
+mov [r15 + 0x123456], r13
+mov [r15 - 0x70], r13
+mov [r15], r14
+mov [r15 + 0x123456], r14
+mov [r15 - 0x70], r14
+mov [r15], r15
+mov [r15 + 0x123456], r15
+mov [r15 - 0x70], r15
