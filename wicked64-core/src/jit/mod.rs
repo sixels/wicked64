0464bc3f60c805a109b33ec4994d60452049d7f5
Sixels <sixels@protonmail.com>
2022-10-17 16:42:12 +0000

    Move JitEngine to `mod.rs`

--- wicked64-core/src/jit/mod.rs
+++ wicked64-core/src/jit/mod.rs
@@ -1,4 +1,28 @@
+use std::{cell::RefCell, rc::Rc};
+
+use crate::n64::State;
+
+use self::compiler::{Compiler, ExecBuffer};
+
+mod bridge;
 pub(crate) mod cache;
-pub mod engine;
+pub mod compiler;
 pub(crate) mod register;
 pub mod state;
+
+/// JIT codegen engine
+#[derive(Debug, Default)]
+pub struct JitEngine {}
+
+impl JitEngine {
+    pub fn new() -> JitEngine {
+        Self::default()
+    }
+
+    pub fn compile_block(&self, state: Rc<RefCell<State>>, cycles: usize) -> (ExecBuffer, usize) {
+        let pclock_size = 5;
+        let pclocks = cycles * pclock_size;
+
+        Compiler::new(state).compile(pclocks)
+    }
+}
