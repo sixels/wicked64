3a06a2967c88ddc7ee93d6ef9423c89ad88684c0
Sixels <sixels@protonmail.com>
2022-10-19 23:04:51 +0000

    Move raw asm code things to `code.rs`

--- wicked64-core/src/jit/cache.rs
+++ wicked64-core/src/jit/cache.rs
@@ -2,32 +2,13 @@ use std::rc::Rc;
 
 use crate::utils::btree_range::BTreeRange;
 
-use super::compiler::ExecBuffer;
-
-#[derive(Clone)]
-pub struct CompiledBlock {
-    exec_buf: ExecBuffer,
-}
-
-impl CompiledBlock {
-    pub fn new(buf: ExecBuffer) -> Self {
-        Self { exec_buf: buf }
-    }
-
-    pub fn execute(&self) {
-        unsafe { self.exec_buf.execute() };
-    }
-}
+use super::code::CompiledBlock;
 
 pub struct Cache {
     blocks: BTreeRange<Rc<CompiledBlock>>,
 }
 
 impl Cache {
-    pub fn new() -> Self {
-        todo!()
-    }
-
     /// Get a compiled block from the cache or create if no entries were found
     pub fn get_or_insert_with<F>(&mut self, addr: usize, mut f: F) -> Rc<CompiledBlock>
     where
@@ -37,16 +18,6 @@ impl Cache {
             return block.clone();
         }
 
-        // let (block, len) = {
-        //     tracing::debug!("Generating cache for address '0x{addr:08x}'");
-
-        //     let cycles = 1024usize;
-
-        //     let (buf, len) = compiler.compile(cycles);
-
-        //     tracing::debug!("Generated code: {:02x?}", buf.as_slice());
-        //     (Rc::new(CompiledBlock::new(buf)), len)
-        // };
         let (block, len) = f();
         let block = Rc::new(block);
         self.blocks.insert(addr..=addr + len, block.clone());
---
+++ wicked64-core/src/jit/code.rs
@@ -0,0 +1,100 @@
+use std::{arch::asm, cell::RefCell, rc::Rc};
+
+use crate::n64::State;
+
+#[derive(Clone)]
+pub struct CompiledBlock {
+    exec_buf: ExecBuffer,
+}
+
+impl CompiledBlock {
+    pub fn new(buf: ExecBuffer) -> Self {
+        Self { exec_buf: buf }
+    }
+
+    pub fn execute(&self) {
+        unsafe { self.exec_buf.execute() };
+    }
+
+    pub fn ptr(&self) -> *const u8 {
+        self.exec_buf.ptr()
+    }
+}
+
+#[derive(Clone)]
+pub struct ExecBuffer {
+    ptr: *const u8,
+    buf: Vec<u8>,
+    state: Rc<RefCell<State>>,
+}
+
+impl ExecBuffer {
+    pub unsafe fn new(buffer: Vec<u8>, state: Rc<RefCell<State>>) -> region::Result<Self> {
+        let ptr = buffer.as_ptr();
+
+        region::protect(ptr, buffer.len(), region::Protection::READ_WRITE_EXECUTE)?;
+
+        Ok(Self {
+            buf: buffer,
+            ptr,
+            state,
+        })
+    }
+
+    pub unsafe fn execute(&self) {
+        let fn_ptr = self.ptr;
+        execute(&self.state, fn_ptr as usize);
+    }
+
+    pub fn ptr(&self) -> *const u8 {
+        self.ptr
+    }
+
+    pub fn as_slice(&self) -> &[u8] {
+        self.buf.as_slice()
+    }
+}
+
+/// Executes the generated code
+///
+/// # Safety
+/// This function relies on inline assembly to setup the stack frame before
+/// jumping into the memory containing the generated code.
+/// It is expected that the code jumps back to the address saved in `r13`
+/// register.
+pub unsafe fn execute(state: &Rc<RefCell<State>>, resume_addr: usize) {
+    let state = state.borrow_mut();
+    let state_addr = (&*state) as *const _ as u64;
+
+    asm!(
+        "lea r13, [rip+3]", // save the address of the instruction after `jmp` as a return address
+        "jmp r14",
+
+        out("r13") _,
+        out("r15") _,
+        in("r14") resume_addr,
+        in("rsi") state_addr,
+    );
+}
+
+/// Resumes the generated code
+///
+/// # Safety
+/// This function relies on inline assembly to setup the stack frame before
+/// jumping into the memory containing the generated code.
+/// It is expected that the code jumps back to the address saved in `r13`
+/// register.
+pub unsafe fn resume(state: &Rc<RefCell<State>>, resume_addr: usize, jump_to: usize) {
+    let state = state.borrow_mut();
+    let state_addr = (&*state) as *const _ as u64;
+
+    std::arch::asm!(
+        "lea r13, [rip+3]", // save the address of the instruction after `jmp` as a return address
+        "jmp r14",
+
+        out("r13") _,
+        in("r15") jump_to,
+        in("r14") resume_addr,
+        in("rsi") state_addr,
+    );
+}
