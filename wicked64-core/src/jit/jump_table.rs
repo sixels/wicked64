f4b2d47742451dbbaee6a22fa9f2450435330322
Sixels <sixels@protonmail.com>
2022-10-23 23:19:51 +0000

    Improve `JumpTable` API

--- wicked64-core/src/jit/jump_table.rs
+++ wicked64-core/src/jit/jump_table.rs
@@ -6,11 +6,12 @@ use super::code::CompiledBlock;
 
 pub struct JumpEntry {
     /// The address to jump
-    pub jump_to: usize,
+    pub target_block: usize,
 }
 
 pub struct JumpTable {
-    table: HashMap<usize, JumpEntry>,
+    /// Maps a n64 physical address to a `JumpEntry`
+    table: HashMap<u64, Option<JumpEntry>>,
 }
 
 impl JumpTable {
@@ -20,20 +21,19 @@ impl JumpTable {
         }
     }
 
-    pub fn get(&mut self, jump_addr: usize) -> &JumpEntry {
-        self.table
-            .entry(jump_addr)
-            .or_insert(JumpEntry { jump_to: 0 })
+    pub fn get(&mut self, phys_jump_addr: u64) -> Option<&JumpEntry> {
+        self.table.entry(phys_jump_addr).or_default().as_ref()
     }
 
-    pub(crate) fn resolve_to(&mut self, addr: usize, block: &Rc<CompiledBlock>) -> usize {
-        if let Some(entry) = self.table.get_mut(&addr) {
-            if entry.jump_to == 0 {
-                entry.jump_to = block.ptr() as usize;
-            }
-            entry.jump_to
-        } else {
-            0
-        }
+    pub(crate) fn resolve_with_block(
+        &mut self,
+        phys_addr: u64,
+        block: &Rc<CompiledBlock>,
+    ) -> Option<&JumpEntry> {
+        self.table.get_mut(&phys_addr).map(|entry| {
+            entry.get_or_insert(JumpEntry {
+                target_block: block.ptr() as usize,
+            }) as &_
+        })
     }
 }
