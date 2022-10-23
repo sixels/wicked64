5b33f50e9b5010f3748f14e544247ae8efb164fa
Sixels <sixels@protonmail.com>
2022-10-23 23:22:07 +0000

    Implement `Interruption::take` to mimic `Option::take`

--- wicked64-core/src/jit/interruption.rs
+++ wicked64-core/src/jit/interruption.rs
@@ -4,3 +4,12 @@ pub enum Interruption {
     None,
     PrepareJump(u64),
 }
+
+impl Interruption {
+    #[must_use]
+    pub fn take(&mut self) -> Interruption {
+        let int = *self;
+        *self = Interruption::None;
+        int
+    }
+}
