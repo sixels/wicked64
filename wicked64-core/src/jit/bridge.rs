3972f9ab5fb3f0cc1649d71816e999fa7e28561d
Sixels <sixels@protonmail.com>
2022-10-23 23:11:04 +0000

    add `get_host_jum_addr` and `get_rip_value` functions

--- wicked64-core/src/jit/bridge.rs
+++ wicked64-core/src/jit/bridge.rs
@@ -1,6 +1,10 @@
+use std::arch::asm;
+
 use crate::{mmu::MemoryUnit, n64::State};
 
-pub extern "C" fn mmu_store(state: &mut State, virt_addr: usize, rt: u64) {
+use super::jump_table::JumpTable;
+
+pub extern "C" fn mmu_store(state: &mut State, virt_addr: u64, rt: u64) {
     let State {
         cpu,
         mmu,
@@ -8,10 +12,27 @@ pub extern "C" fn mmu_store(state: &mut State, virt_addr: usize, rt: u64) {
         ..
     } = state;
 
-    let phys_addr = cpu.translate_virtual(virt_addr);
+    let phys_addr = cpu.translate_virtual(virt_addr) as usize;
 
     // invalidate 8 bytes starting from `phys_addr`
     *cache_invalidation = Some((phys_addr, phys_addr + 8 + 1));
 
     mmu.store::<_, byteorder::BigEndian>(phys_addr, rt);
+}
+
+pub extern "C" fn get_host_jump_addr(state: &mut State, jump_table: &mut JumpTable, n64_addr: u64) {
+    let _ = jump_table.get(state.cpu.translate_virtual(n64_addr));
+}
+
+#[naked]
+pub extern "C" fn get_rip_value(disp: u32) -> u64 {
+    unsafe {
+        #[rustfmt::skip]
+        asm!(
+            "mov rax, [rsp]",
+            "add rax, rdi",
+            "ret",
+            options(noreturn),
+        );
+    }
 }
